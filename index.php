53f0cb83f2e9d61a2d59f9b001051ec310d32150
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-16 17:36:35 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test Your Irregular Verbs Knowledge</title>
+	<link rel="stylesheet" href="src/style.css">
+</head>
+<body>
+
+	<div class="container">
+
+
+	</div><!-- .container -->
+
+<script src="src/script.js"></script>
+</body>
+</html>
