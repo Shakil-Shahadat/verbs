c2d93e5b7abbe6329594355a99327579a078256a
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-16 17:40:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,24 @@
 
 	<div class="container">
 
+		<table>
+			<tr>
+				<th>Serial</th>
+				<th>Verb</th>
+				<th>Past Tense</th>
+				<th>Past Participle</th>
+				<th>Check</th>
+			</tr>
+			<tr>
+				<td></td>
+				<td>Go</td>
+				<td data-answer="went" contenteditable></td>
+				<td data-answer="gone" contenteditable></td>
+				<td>
+					<button onclick="checkAnswer( this.parentElement.parentElement )">Check</button>
+				</td>
+			</tr>
+		</table>
 
 	</div><!-- .container -->
 
