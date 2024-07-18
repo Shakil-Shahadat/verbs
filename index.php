54b73184c620224856dc0c763b4fa80a709e4045
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-18 21:28:37 +0600

    Update index.php

--- index.php
+++ index.php
@@ -146,6 +146,15 @@
 			</tr>
 			<tr>
 				<td></td>
+				<td>Beset</td>
+				<td data-answer="beset" contenteditable></td>
+				<td data-answer="beset" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
 				<td>Go</td>
 				<td data-answer="went" contenteditable></td>
 				<td data-answer="gone" contenteditable></td>
