1dceafe602852055d9c6344641876a44101b3de7
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-19 16:49:11 +0600

    Update index.php

--- index.php
+++ index.php
@@ -891,6 +891,15 @@
 					<button>Check</button>
 				</td>
 			</tr>
+			<tr>
+				<td></td>
+				<td>Knit</td>
+				<td data-answer="knitted, knit" contenteditable></td>
+				<td data-answer="knitted, knit" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
 		</table>
 
 	</div><!-- .container -->
