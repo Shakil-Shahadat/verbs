00d0c8edd51874f645a5a39faadd6c2d2b757e33
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-19 17:07:37 +0600

    Update index.php

--- index.php
+++ index.php
@@ -936,6 +936,15 @@
 					<button>Check</button>
 				</td>
 			</tr>
+			<tr>
+				<td></td>
+				<td>Lean</td>
+				<td data-answer="leant, leaned" contenteditable></td>
+				<td data-answer="leant, leaned" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
 		</table>
 
 	</div><!-- .container -->
