b9c5c679d683d3bd515035a58abe12d5858b3c88
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-19 19:39:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1197,6 +1197,15 @@
 					<button>Check</button>
 				</td>
 			</tr>
+			<tr>
+				<td></td>
+				<td>Outshine</td>
+				<td data-answer="outshone" contenteditable></td>
+				<td data-answer="outshone" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
 		</table>
 
 	</div><!-- .container -->
