0de8a68566a1661a3803d19628cc6895c17300b5
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-19 11:48:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -218,6 +218,15 @@
 			</tr>
 			<tr>
 				<td></td>
+				<td>Blend</td>
+				<td data-answer="blended, blend" contenteditable></td>
+				<td data-answer="blended, blend" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
 				<td>Go</td>
 				<td data-answer="went" contenteditable></td>
 				<td data-answer="gone" contenteditable></td>
