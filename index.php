dfd62dd5b70f89a72d1ff860f786f334c795f6c9
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-19 13:46:21 +0600

    Update index.php

--- index.php
+++ index.php
@@ -677,6 +677,15 @@
 			</tr>
 			<tr>
 				<td></td>
+				<td>Forswear</td>
+				<td data-answer="forswore" contenteditable></td>
+				<td data-answer="forsworn" contenteditable></td>
+				<td>
+					<button>Check</button>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
 				<td>Go</td>
 				<td data-answer="went" contenteditable></td>
 				<td data-answer="gone" contenteditable></td>
